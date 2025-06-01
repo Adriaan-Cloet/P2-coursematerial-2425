a834e6ef27bbaec313772a10ba51d4382c6c530f
Adriaan <r0928701@student.ucll.be>
2025-06-01 14:34:12 +0200

    random push

--- 01-oo/03-properties/assignments/04-bmi-calculator/student.py
+++ 01-oo/03-properties/assignments/04-bmi-calculator/student.py
@@ -1,1 +1,28 @@
-# Write your code here
+from math import sqrt
+
+
+class BMICalculator:
+    def __init__(self, weight_in_kg, height_in_m):
+        self.__weight_in_kg = weight_in_kg
+        self.__height_in_m = height_in_m
+
+    @property
+    def weight_in_kg(self):
+        return self.__weight_in_kg
+    
+    @property
+    def height_in_m(self):
+        return self.__height_in_m
+    
+    @property
+    def bmi(self):
+        return self.__weight_in_kg / (self.__height_in_m ** 2)
+    
+    @property
+    def category(self):
+        if self.bmi < 18.5:
+            return "underweight"
+        elif 18.5 <= self.bmi < 25:
+            return "normal"
+        else:
+            return "overweight"
--- 01-oo/03-properties/assignments/06-time/student.py
+++ 01-oo/03-properties/assignments/06-time/student.py
@@ -1,1 +1,27 @@
-# Write your code here
+class Time:
+    def __init__(self, hours, minutes, seconds):
+        self.__hours = hours
+        self.__minutes = minutes
+        self.__seconds = seconds
+
+    @property
+    def hours(self):
+        if self.__hours < 0 or self.__hours > 24:
+            raise ValueError("Hours must be between 0 and 24")
+        return self.__hours
+    
+    @property
+    def minutes(self):
+        if self.__minutes < 0 or self.__minutes > 60:
+            raise ValueError("Minutes must be between 0 and 60")
+        return self.__minutes
+    
+    @property
+    def seconds(self):
+        if self.__seconds < 0 or self.__seconds > 60:
+            raise ValueError("Seconds must be between 0 and 60")
+        return self.__seconds
+    
+        
+    
+    
