61c5f4b09a21a16d5afc4254b327a61a0c313319
Adriaan-Cloet <r0928701@ucll.be>
2025-06-12 20:46:45 +0200

    commit

--- 03-testing/04-exceptions/tests.py
+++ 03-testing/04-exceptions/tests.py
@@ -16,6 +16,22 @@ def test_valid_creation(title, isbn):
     assert book.isbn == isbn
 
 @pytest.mark.parametrize(
-    'title',
-    
-)
+    'title', [
+        "",
+])
+
+def test_creation_with_invalid_title(title):
+    with pytest.raises(RuntimeError):
+        Book(title, "987-1779501127")
+
+@pytest.mark.parametrize("isbn", [
+    "978-1779501128",      # incorrect checksum
+    "978-177950112",       # only 12 digits
+    "978-17795011278",     # 14 digits
+    "978-17795011A7",      # contains letter
+    "",                    # empty string
+])
+
+def test_with_invalid_isbn(isbn):
+    with pytest.raises(RuntimeError):
+        Book("Valid Title", isbn)
